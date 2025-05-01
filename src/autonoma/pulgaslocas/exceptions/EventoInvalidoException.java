21b8047ca0af3c630c045955d5c2543bbdf16de2
mateo <mateoq152006@gmail.com>
2025-05-01 18:47:47 -0500

    se documenta ImpactoFueraDeRangoException

--- src/autonoma/pulgaslocas/exceptions/EventoInvalidoException.java
+++ src/autonoma/pulgaslocas/exceptions/EventoInvalidoException.java
@@ -6,7 +6,10 @@ package autonoma.pulgaslocas.exceptions;
 
 /**
  *
- * @author mateo
+ * @author Mateo Quintero <mateo.quinterom@autonoma.edu.co>
+ * @since 20250501
+ * @see autonoma.pulgaslocas.exceptions
+ * @version 1.0.0
  */
 public class EventoInvalidoException extends RuntimeException{
     
