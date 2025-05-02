0f517617bc557fdcdf94b1faf81d17b32b046b25
mateo <mateoq152006@gmail.com>
2025-05-01 20:30:48 -0500

    se crea los metodos de la clase campo de battala

--- src/autonoma/pulgaslocas/elements/CampoDeBatalla.java
+++ src/autonoma/pulgaslocas/elements/CampoDeBatalla.java
@@ -18,7 +18,7 @@ public class CampoDeBatalla {
     //Atributos
     private int ancho;
     private int alto;
-    private ArrayList<Pulga>;
+    private ArrayList <Pulga> pulgas;
     public CampoDeBatalla(int ancho, int alto) {
         this.ancho = ancho;
         this.alto = alto;
@@ -33,13 +33,24 @@ public class CampoDeBatalla {
         return alto;
     }
     
-    //metodos
+    //metodos de campo
     
     public void agrgarPluga(){
+        
+        Pulga p = new Pulga(1,2,3,4);
+        
+        pulgas.add(p);
     }
     
-    public void removerPluga{
+    public void removerPluga(){
+        Pulga p = new Pulga(1,2,3,4);
+        pulgas.remove(p);
+
     }
+    
+    public void actualizarEstado(){
+    }
+    
     
     
     
