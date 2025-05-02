d926f838245b5ad2f1c6bbdb3ffc3852d8f86362
mateo <mateoq152006@gmail.com>
2025-05-01 20:08:26 -0500

    se crea la clase de campo de batalla poniendo atributos y metodos de acceso

---
+++ src/autonoma/pulgaslocas/elements/CampoDeBatalla.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pulgaslocas.elements;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mateo Quintero <mateo.quinterom@autonoma.edu.co>
+ * @since 20250501
+ * @see autonoma.pulgaslocas.elements
+ * @version 1.0.0
+ */
+public class CampoDeBatalla {
+    
+    //Atributos
+    private int ancho;
+    private int alto;
+    private ArrayList<Pulga>;
+    public CampoDeBatalla(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    ///Metodos accesos (get)
+    public int getAncho() {
+        return ancho;
+    }
+
+    public int getAlto() {
+        return alto;
+    }
+    
+    //metodos
+    
+    public void agrgarPluga(){
+    }
+    
+    public void removerPluga{
+    }
+    
+    
+    
+}
